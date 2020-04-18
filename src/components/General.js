d2ba5aca05af0219a12b03b7433469ae7553b48b
Ruben <ruben.brami@free.fr>
2020-04-18 16:56:16 +0200

    Added function to clarify

--- src/components/General.js
+++ src/components/General.js
@@ -1,5 +1,13 @@
 import { h } from 'hyperapp'
 
+const chooseGraphToShow = (props, dataNumber) => {
+  return {
+    title: props.datasets[dataNumber] === undefined ? 'Ooops, on dirait que vous n\'avez pas récupéré les données : ce graph ne veut rien dire' : 'Humidité de l\'air à chatelet en fonction de la date',
+    labels: props.datasets[dataNumber] === undefined ? props.datasets[0].data[1] : props.datasets[dataNumber].data[1],
+    data: props.datasets[dataNumber] === undefined ? props.datasets[0].data[0] : props.datasets[dataNumber].data[0]
+  }
+}
+
 export const general = (props, graphActions) =>
   h('div', {}, [
     h('fieldset', {}, [
@@ -7,9 +15,8 @@ export const general = (props, graphActions) =>
       h('label', {}, [
         h('input', {
           type: 'button',
-          onclick: [graphActions.state.addBar, { title: props.datasets[1] === undefined ? 'Ooops, on dirait que vous n\'avez pas récupéré les données : ce graph ne veut rien dire' : 'Humidité de l\'air à chatelet en fonction de la date', labels: props.datasets[1] === undefined ? props.datasets[0].data[1] : props.datasets[1].data[1], data: props.datasets[1] === undefined ? props.datasets[0].data[0] : props.datasets[1].data[0] }]
-        }
-        ),
+          onclick: [graphActions.state.addBar, chooseGraphToShow(props, 1)]
+        }),
         h('span', null, 'État: ajouter le graph humidité'),
         h('br', null)
       ])
