a9201b6fdc00f1970896a3788475392b13219b3b
Gaelle Vallet <gaelle.vallet58@gmail.com>
2020-04-19 15:47:21 +0200

    Data design
    arboresence data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -4,6 +4,28 @@ export const data = (props, dataActions) =>
   h('div', {}, [
     h('fieldset', {}, [
       h('legend', {}, 'Récupération des données'),
+      h('p', {}, 'Choix de la Station'),
+      h('input', { 
+        type: 'checkbox', 
+        id: 'Station01'
+      }),
+      h('label', { for: 'Station01' }, 'Station01'),
+      h('input', { 
+        type: 'checkbox', 
+        id: 'Station02'
+      }),
+      h('label', { for: 'Station02' }, 'Station02'),
+      h('input', { 
+        type: 'checkbox', 
+        id: 'Station03'
+      }),
+      h('label', { for: 'Station03' }, 'Station03'),
+
+      h('p', {}, 'Date de Début'),
+      h('input', { type: 'date' }),
+      h('p', {}, 'Date de Fin'),
+      h('input', { type: 'date' }),
+
       h('label', {}, [
         h('input', {
           type: 'button',
@@ -16,7 +38,7 @@ export const data = (props, dataActions) =>
               }),
             props.datasetIds.chatelet,
             10)
-        }),
+        }, 'ok'),
         h('br', null)
       ])
     ])
